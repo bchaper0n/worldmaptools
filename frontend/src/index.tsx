1a6b860750e49bf6d0ac1ddabf4934e9246acd00
bchaper0n <brandon.chaperon@mail.mcgill.ca>
2025-09-19 15:54:25 +0200

    javascript to typescript

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(
   <h1>hello world!</h1>
 );
